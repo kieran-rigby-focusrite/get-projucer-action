30a4ac7b7e0c8bb821281c53b324805106975b1d
kieran-rigby-focusrite <56405766+kieran-rigby-focusrite@users.noreply.github.com>
2020-07-08 14:55:20 +0100

    Removed debug output from check-for-changes.sh

--- scripts/check-for-changes.sh
+++ scripts/check-for-changes.sh
@@ -1,12 +1,8 @@
-git fetch
+git fetch > /dev/null 2>&1
 #CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 # For now, just assume we are on master branch
 HEAD_HASH=$(git rev-parse HEAD)
 UPSTREAM_HASH=$(git rev-parse master@{upstream})
-
-echo "Current branch: $CURRENT_BRANCH"
-echo "Head Hash: $HEAD_HASH"
-echo "Upstream Hash: $UPSTREAM_HASH"
 
 if [ "$HEAD_HASH" != "$UPSTREAM_HASH" ] 
 then
