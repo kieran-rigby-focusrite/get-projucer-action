bf62584b2629c63dd061a63b5ae166adc3e464b7
Kieran Rigby <kieran.rigby@focusrite.com>
2020-07-09 20:36:25 +0100

    Added python script to grab latest hash for given ref

---
+++ scripts/get-hash-from-ref.py
@@ -0,0 +1,14 @@
+import subprocess
+import argparse
+
+def get_hash_from_ref (url, ref):
+    command_output = subprocess.check_output (["git", "ls-remote", url, ref])
+    return command_output.split ("\t")[0]
+
+parser = argparse.ArgumentParser(description='Retrieve the latest hash from a given git ref')
+
+parser.add_argument('URL', help='The URL of the repository to fetch the hash from')
+parser.add_argument('ref', help='The ref that you would like to retrieve the latest hash for')
+args = parser.parse_args()
+
+print (get_hash_from_ref (args.URL, args.ref))
